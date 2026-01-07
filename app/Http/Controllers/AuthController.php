8f24fee52fdaade03eb71acb99741ff5d46b142d
thanakarthikzerosoft-source <thanakarthik.zerosoft@gmail.com>
2026-01-07 10:49:07 +0530

    Initial Laravel project setup

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password) // encrypt
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'data' => $user
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Login successful',
+            'data' => $user
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\AuthController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/db-test', function () {
+    return DB::select('SELECT * FROM leads');
 });
+Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
