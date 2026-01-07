bc47ad9ba4c8886b78a3073c8c855a75c49f3954
thanakarthikzerosoft-source <thanakarthik.zerosoft@gmail.com>
2026-01-07 15:18:16 +0530

    cleared crsf tocken issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken; #used to csrf tocken not check
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        // $middleware->remove(VerifyCsrfToken::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -13,5 +14,13 @@ Route::get('/db-test', function () {
 });
 Route::get('/users', [UserController::class, 'index']);
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
+
+// Route::post('/register', [AuthController::class, 'register'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+// Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::post('/register', [AuthController::class, 'register'])
+    ->withoutMiddleware([VerifyCsrfToken::class]);
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->withoutMiddleware([VerifyCsrfToken::class]);
